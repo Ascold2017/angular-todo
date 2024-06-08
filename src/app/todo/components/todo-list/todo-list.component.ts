a6d0a428b39e6b06d34d638bf5826cc724afc52d
Mihail Pogosyan <mihail.pogosyan96@gmail.com>
2024-06-08 19:23:35 +0400

    update todo list layout

--- src/app/todo/components/todo-list/todo-list.component.ts
+++ src/app/todo/components/todo-list/todo-list.component.ts
@@ -4,7 +4,7 @@ import {TodoService} from "../../todo.service";
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.css'
+  styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
   constructor(public todoService: TodoService) {
